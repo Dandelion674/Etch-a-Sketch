97b6f0ef5398ab0154ec912707dcbee5c3084d0d
Kelson Neto <kelsonneto674@gmail.com>
2025-09-15 16:37:45 +0100

    feat(js): implement grid creation

---
+++ script.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('#container');
+const newGridBtn = document.querySelector('#new-grid-btn');
+
+const containerSize = 500; 
+
+container.style.width = `${containerSize}px`;
+container.style.height = `${containerSize}px`;
+
+//Grid Creation Func
+function createGrid(size) {
+    
+    container.innerHTML = '';
+    
+    const squareSize = containerSize / size;
+    
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        
+        
+        square.addEventListener('mouseenter', function() {
+            this.style.backgroundColor = '#000000ff';
+        });
+        
+        container.appendChild(square);
+    }
+}
+
+createGrid(16);
